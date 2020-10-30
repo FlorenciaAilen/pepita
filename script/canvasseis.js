2966b895b61985c6edd3a47012a9dc2c70fa0944
Janneth0 <churajanneth@gmail.com>
2020-10-30 05:19:33 -0300

    Nivel 6

--- script/canvasseis.js
+++ script/canvasseis.js
@@ -1,54 +1,56 @@
+
 /**************   INICIO    ***************/
-imgNave = new Image();
-imgOvni = new Image();
-imgOvni.src = "img/seis/ALIEN.png";
-imgNave.src = "img/seis/FIN2.png";
-
-// var audio = document.getElementById("dispara");
+canvas = document.getElementById("canvaseis");
+canvas.width = window.innerWidth * 0.7;
+canvas.height = window.innerHeight * 0.8;
+canvas.style.background = "black";
 
 
+function resizeCanvas() {
+	canvas.width = window.innerWidth * 0.7;;
+	canvas.height = window.innerHeight * 0.8;
+}
+window.addEventListener("resize", resizeCanvas)
 
-window.onload = function () {
-	canvas = document.getElementById("canvaseis");
+
+// -------------------------------------------------------------------------------
+function load() {
 	if (canvas && canvas.getContext) {
+
 		ctx = canvas.getContext("2d");
+		principal()
+
 		if (ctx) {
 			x = canvas.width / 2;
-			iniciando()
-		} else {
+
+			// imgNave = new Image();
+			imgOvni = new Image();
+			imgOvni.src = "img/ALIEN.png";
+			// imgNave.src = "img/seis/FIN2.png";
+			obteneroutfit()
+			imgNave = new Image() //ACA PONEN EL NOMBRE DE LA VARIABLE DE PEPITA QUE LE PUSIERON
+			imgNave.src = url;
+			imgNave.onload = function () {
+				nave = new nave(canvas.width);
+			}
+			imgOvni.onload = function () {
+				for (var i = 0; i < 5; i++) {
+					for (var j = 0; j < 10; j++) {
+						ovnis_array.push(new Enemigo(canvas.width / 5 + (canvas.width / 15) * j, canvas.height / 8 + (canvas.width / 25) * i));
+					}
+				}
+				setTimeout(anima, 1500);
+				disparoEnemigo = setTimeout(disparaEnemigo, tiempoDisparo);
+			}
+
+		}
+		else {
 			alert("Error al crear tu contexto");
 		}
 	}
 }
 
-function iniciando() {
-	canvas.width = window.innerWidth * 0.7;
-
-	canvas.height = window.innerHeight * 0.8;
-	console.log(canvas.height)
-	canvas.style.background = "black";
-	// canvas.style.backgroundImage = 'url("imagenes/espacio.gif")';
-	// canvas.style.backgroundSize = 'cover';
-	x = (canvas.width / 2) - 30;
-
-	mensaje("COMIENZA EL JUEGO");
-	setTimeout(mensaje, 300)
-
-	nave = new nave(0);
-
-	for (var i = 0; i < 5; i++) {
-		for (var j = 0; j < 10; j++) {
-			ovnis_array.push(new Enemigo(100 + 55 * j, 35 + 40 * i));
-		}
-	}
-	setTimeout(anima, 100);
-	disparoEnemigo = setTimeout(disparaEnemigo, tiempoDisparo);
-
-}
-
-
 /*************  LISTENER  **************/
-
 document.addEventListener("keydown", function (e) {
 	teclaPulsada = e.keyCode;
 	tecla[e.keyCode] = true;
@@ -58,7 +60,11 @@ document.addEventListener("keyup", function (e) {
 });
 
 /******************* VARIABLES ********************/
+let url
 var canvas, ctx;
+let contador = 0;
+let nro = 0;
+let vida = 3;
 var x = 100;
 var y = 100;
 var teclaIzquierda = 37;
@@ -81,9 +87,8 @@ var disparoEnemigo;
 var tiempoDisparo = 1000; //Del Enemigo
 var puntos = 0;
 var sound = document.getElementById("dispara");
-/*****************
-OBJETOS
-******************/
+
+/***************** OBJETOS ******************/
 function Bala(x, y, w) {
 	this.x = x;
 	this.y = y;
@@ -105,33 +110,34 @@ function Bala(x, y, w) {
 }
 function nave(x) {
 	this.x = x;
-	this.y = 400;
-	this.w = 70;
-	this.h = 35;
+	this.y = canvas.height / 1.2;
+	this.w = canvas.width / 15;
+	console.log(this.w)
+	this.h = 45;
 	this.dibuja = function (x) {
 		this.x = x;
 		if (imgAni2 < 5) {
-			ctx.drawImage(imgNave, 0, 0, 300, 350, this.x, this.y, 100, 100);
+			ctx.drawImage(imgNave, 0, 0, 300, 350, this.x, this.y, canvas.width / 14, canvas.height / 8);
 			imgAni2 = imgAni2 + 1;
 			imgAni = imgAni + 1;
 			checarBalas();
 			//setInterval(checarBalas(),1000);
 		} else if (imgAni2 < 10) {// 600, 0, 300, 350, this.x, this.y, 60,80);
-			ctx.drawImage(imgNave, 300, 0, 300, 350, this.x, this.y, 100, 100);
+			ctx.drawImage(imgNave, 300, 0, 300, 350, this.x, this.y, canvas.width / 14, canvas.height / 8);
 			imgAni2 = imgAni2 + 1;
 			imgAni = imgAni + 1;
 		} else {
-			ctx.drawImage(imgNave, 300, 0, 300, 350, this.x, this.y, 100, 100);
+			ctx.drawImage(imgNave, 300, 0, 300, 350, this.x, this.y, canvas.width / 14, canvas.height / 8);
 			imgAni2 = 0;
 		}
 
+
 	};
 }
-
 function Enemigo(x, y) {
 	this.x = x;
 	this.y = y;
-	this.w = 50;
+	this.w = canvas.width / 15;
 	this.veces = 0;
 	this.dx = 5;
 	this.ciclos = 0;
@@ -176,25 +182,67 @@ function Enemigo(x, y) {
 
 	};
 }
-/*****************
-FUNCIONES
-******************/
-function anima() {
+
+/***************** FUNCIONES ******************/
+function principal() {//MUESTRA EL MENSAJE DEL INICIO-PRINCIPAL
+	mensaje("COMIENZA EL JUEGO");
+	ctx.fillText("Tenes 3 Vidas", canvas.width / 4.4, canvas.height / 2, canvas.width, 10);
+	ctx.fillText("Cantidad de Balas: 100", canvas.width / 30, canvas.height / 1.5);
+}
+function anima() {//ANIMA Y CONTROLA LA COLISION CON BALAS
+
 	if (endGame == false) {
-		requestAnimationFrame(anima);
+
 		verifica();
 		pinta();
 		colisiones();
+
+		requestAnimationFrame(anima);
+		for (var j = 0; j < balasEnemigas_array.length; j++) {
+			bala = balasEnemigas_array[j];
+			if (bala != null) {
+				if ((bala.x > nave.x) &&
+					(bala.x < nave.x + nave.w) &&
+					(bala.y > nave.y) &&
+					(bala.y < nave.y + nave.h)) {
+					nro++;
+
+
+					controlando()
+
+				}
+
+			}
+		}
+
 	}
 }
-function mensaje(cadena) {
-	var lon = (canvas.width - (50 * cadena.length)) / 2;
-	ctx.fillStyle = "white";
-	ctx.clearRect(20, 20, canvas.width, canvas.height);
-	ctx.font = "bold 75px Arial";
-	ctx.fillText(cadena, lon, 100);
+function controlando() {//CONTROLA LA CANTIDAD DE VIDAS
+	contador++;
+	console.log("contador", contador, "nro", nro);
+	if (contador == 11) {
+		vida--;
+		playVida()
+		console.log("vida", vida)
+		live()
+	} else if (contador == 22) {
+		vida--
+		console.log("vida", vida)
+		playVida()
+	} else if (contador == 33) {
+		playVida()
+		playGameOver()
+		gameOver()
+	}
 }
-function colisiones() {
+function mensaje(cadena) {//FUNCION UTILIZADA PARA MESAJES 
+	var lon = (canvas.width) * cadena.length / 100;
+	ctx.fillStyle = "white";
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.font = "bold 50px 'Press Start 2P', cursive ";
+	ctx.fillText(cadena, lon, (canvas.height) / 4);
+}
+function colisiones() {//COLISION DE BALA CON ENEMIGO
 	for (var i = 0; i < ovnis_array.length; i++) {
 		for (var j = 0; j < balas_array.length; j++) {
 			enemigo = ovnis_array[i];
@@ -214,68 +262,84 @@ function colisiones() {
 			}
 		}
 	}
-	for (var j = 0; j < balasEnemigas_array.length; j++) {
-		bala = balasEnemigas_array[j];
-		if (bala != null) {
-			if ((bala.x > nave.x) &&
-				(bala.x < nave.x + nave.w) &&
-				(bala.y > nave.y) &&
-				(bala.y < nave.y + nave.h)) {
-				gameOver();
-			}
-		}
-	}
 }
-function gameOver() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	balas_array = [];
-	ovnis_array = [];
-	balasEnemigas_array = [];
+function gameOver() {//MUESTRA LOS CARTELES CORRESPONDIENTES
+
 
 	if (enemigosVivos == 0) {
-		mensaje("GANASTE");
-		var img = new Image();
-		img.src = "img/seis/marcianito_family.png";
-		img.onload = function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		balas_array = [];
+		ovnis_array = [];
+		balasEnemigas_array = [];
+		ctx.fillStyle = "white";
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.font = "bold 50px 'Press Start 2P', cursive ";
+		ctx.fillText("  GANASTE ", canvas.width/3.5, (canvas.height) / 4);
+		var imgGana = new Image();
+		imgGana.src = "img/marcianito_family.png";
+		imgGana.onload = function () {
 
-			ctx.drawImage(img, 250, 200, 10, 10, 10, 10, 10);
+			ctx.drawImage(imgGana, (canvas.width) / 2.7, canvas.height/3);
 		}
-	} else {
-		mensaje("GAME OVER");
-		var img = new Image();
-		img.src = "img/seis/marcianito_llora.png";
-		img.onload = function () {
-			ctx.drawImage(img, 250, 200);
-		}
-		// canvas.style.backgroundImage = 'url("imagenes/BACK.png")';
-		canvas.style.backgroundSize = 'cover';
-		
-		Raphael(0, 0, 0, 0).rect(0, 0, 50, 50)
-			.attr({ fill: "#000" })
-			.click(function () {
-				alert('second rectangle clicked');
-			});
+		// finalNivel();
 	}
+	else if (contador == 33) {
+		ctx.fillStyle = "white";
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.font = "bold 50px 'Press Start 2P', cursive ";
+		ctx.fillText("GAME OVER", canvas.width/3.55, (canvas.height) / 4);
+		var imgPierde = new Image();
+		imgPierde.src = "img/marcianito_llora.png";
+		imgPierde.onload = function () {
+			ctx.drawImage(imgPierde, (canvas.width) / 2.5, (canvas.height)/4);
+		}
+
+	}
+	
 	endGame = true;
 	clearTimeout(disparoEnemigo);
+	
 }
-function score() {
+// function final() {
+//     if(contador==33) {
+
+//         console.log("final")
+//         let button = document.getElementById('siguiente')
+//         button.classList.remove('hidden')
+
+//     }
+//  }
+//  final();
+// CORREGIR BOTON FIN
+function score() {//MUESTRA EL SCORE OBTENIDO
 	ctx.save();
 	ctx.fillStyle = "white";
-	ctx.clearRect(0, 0, canvas.width, 20);
-	ctx.font = "bold 12px Courier";
-	ctx.fillText("SCORE: " + puntos, 10, 20);
+	ctx.clearRect(0, 25, canvas.width, 20);
+	ctx.font = "bold 14px 'Press Start 2P', cursive ";
+	ctx.fillText("SCORE: " + puntos, canvas.width / 10, 20);
 	ctx.restore();
 }
-function municiones() {
+function municiones() { //MUESTRA LA CANTIDAD DE DISPAROS QUE NOS QUEDAN
 	ctx.save();
 	ctx.fillStyle = "white";
-	ctx.clearRect(0, 20, canvas.width, 20);
-	ctx.font = "bold 12px Courier";
-	ctx.fillText("Municion: " + municion, 10, 40);
+	ctx.clearRect(0, 60, canvas.width, 40);
+	ctx.font = "bold 14px 'Press Start 2P', cursive ";
+	ctx.fillText("Munición: " + municion, canvas.width / 10, 40);
 	ctx.restore();
 }
-function verifica() {
+function live() {//MUESTRA LAS VIDAS 
+
+	ctx.save();
+	ctx.fillStyle = "white";
+	ctx.clearRect(0, 90, canvas.width, canvas.height / 50);
+	ctx.font = "bold 14px 'Press Start 2P', cursive ";
+	ctx.fillText(vida + " :VIDAS", canvas.width / 1.15, 30);
+
+	ctx.restore();
+
+
+}
+function verifica() {//VERIFICA LAS TECLAS
 	if (tecla[teclaDerecha]) x += 5;
 	if (tecla[teclaIzquierda]) x -= 5;
 	//Verifica cañon
@@ -285,7 +349,7 @@ function verifica() {
 	if (tecla[teclaEspacio]) {
 		// //activar sonido de disparo
 		// sound.onplay()
-
+		playTiro()
 		if (tiempoBala == true && municion != 0) {
 			tiempoBala = false;
 			balas_array.push(new Bala(nave.x + 60, nave.y - 3, 5));
@@ -296,7 +360,7 @@ function verifica() {
 		}
 	}
 }
-function checarBalas() {
+function checarBalas() {//CONTROL DE BALAS
 	var balasArrayVal = 0;
 	for (let i = 0; i < balas_array.length; i++) {
 		if (balas_array[i] != null) {
@@ -305,14 +369,15 @@ function checarBalas() {
 	}
 	if (municion == 0 && balas_array.length == 100 && balasArrayVal == 0 && enemigosVivos > 0) {
 		tecla[teclaEspacio] = false;
-		alert("Sin municion");
+		alert("Sin munición");
 		gameOver();
 	}
 }
-function pinta() {
+function pinta() {//PINTA ENEMIGOS Y BALAS
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	score();
 	municiones();
+	live()
 	nave.dibuja(x);//Esto es lo que hace que se muestre
 	//Balas
 	for (var i = 0; i < 100; i++) {
@@ -341,7 +406,7 @@ function pinta() {
 	}
 	if (numEnemigos == 0) gameOver();
 }
-function disparaEnemigo() {
+function disparaEnemigo() {//DISPARO ALEATORIO DEL ENEMIGO
 	for (var i = ovnis_array.length - 1; i > 0; i--) {
 		if (ovnis_array[i] != null) {
 			ultimos.push(i);
@@ -364,4 +429,41 @@ function disparaEnemigo() {
 	balasEnemigas_array.push(new Bala(ovnis_array[d].x + ovnis_array[d].w / 2, ovnis_array[d].y, 5));
 	clearTimeout(disparoEnemigo);
 	disparoEnemigo = setTimeout(disparaEnemigo, tiempoDisparo);
+}
+
+/***************** AUDIOS ******************/
+function playTiro() {
+	let paso = new Audio("sound/piu.wav");
+	paso.play();
+}
+function playVida() {
+	let paso = new Audio("sound/vida.mp3");
+	paso.play();
+}
+function playGameOver() {
+	let paso = new Audio("sound/GameOver.mp3");
+	paso.play();
+}
+
+// cambia de personajes
+function obteneroutfit() {
+	url = localStorage.getItem("url")
+	switch (url) {
+		case "img/pepitatraje/pepitawonderwoman.png":
+			url = "img/pepitatraje/naveWonder.png";
+			break;
+		case "img/pepitatraje/pepitakillbill.png":
+			url = "img/pepitatraje/naveKillbill.png";
+			break;
+		case "img/pepitatraje/pepitabruja.png":
+			url = "img/pepitatraje/naveBruja.png";
+			break;
+		case "img/pepitatraje/pepitajedi.png":
+			url = "img/pepitatraje/naveJedi.png";
+			break;
+
+		default:
+			url = "img/pepitatraje/navePepita.png";
+			break;
+	}
 }
